98d9102ea18c9fd8f52bf50700fd31636825bedb
SunkavalliSahithi <104184166+SunkavalliSahithi@users.noreply.github.com>
2024-05-22 17:44:42 +0530

    Create README.m
    This is the process of basic quiz game

---
+++ README.m
@@ -0,0 +1,23 @@
+# Basic Quiz Game
+
+This is a simple quiz game built with Python. It asks users a series of questions and evaluates their performance based on their answers.
+
+## Features
+
+- Multiple-choice questions
+- Scoring system
+- User input validation
+- Feedback on answers
+- Final score display
+
+## How to Run
+
+1. Clone the repository:
+
+   git clone https://github.com/SunkavalliSahithi/Basic-quiz-game.git
+   cd basic-quiz-game
+
+
+
+
+
